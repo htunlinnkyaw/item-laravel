3992c1dfa26ed4c37f3644b2ad405be8bb43ae43
htunlinnkyaw <tunlinkyaw4781@gmail.com>
2024-08-31 11:11:17 +0630

    update

---
+++ app/Http/Controllers/Api/ItemApiController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $items = Item::all();
+        return response()->json(["data" => $items, "message" => "data fetching successful"]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        $images = [];
+
+        if ($request->item_images) {
+            foreach ($request->file('item_images') as $file) {
+                $newName = "item_image" . uniqid() . "." . $file->extension();
+                $file->storeAs('public/item_images', $newName);
+                $images[] = $newName;
+            }
+        }
+
+        $item = new Item();
+        $item->name = $request->name;
+        $item->price = $request->price;
+        $item->stock = $request->stock;
+        $item->description = $request->description;
+        $item->status = $request->status;
+        $item->category_id = $request->category_id;
+        $item->item_images = json_encode($images);
+
+        $item->save();
+
+        return response()->json(["message" => "Item is successfully created."]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+
+
+
+        $item = Item::find($id);
+        $item->name = $request->name;
+        $item->price = $request->price;
+        $item->stock = $request->stock;
+        $item->description = $request->description;
+        $item->status = $request->status;
+        $item->category_id = $request->category_id;
+        $item->update();
+
+        return response()->json(["message" => "Item is updated successfully."]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $item = Item::find($id);
+        if ($item) {
+            $item->delete();
+        }
+
+        return response()->json(["message" => "item is successfully deleted."]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\CategoryApiController;
+use App\Http\Controllers\Api\CategoryApiController;
+use App\Http\Controllers\Api\ItemApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +13,11 @@ Route::get("/item", function () {
     return Response()->json("Hello World");
 });
 
-Route::get('/category', [CategoryApiController::class, 'index']);
-Route::post('/category', [CategoryApiController::class, 'store']);
-Route::put('/category/{id}', [CategoryApiController::class, 'update']);
-Route::delete('/category/{id}', [CategoryApiController::class, 'destroy']);
+// Route::get('/category', [CategoryapiController::class, 'index']);
+// Route::post('/category', [CategoryApiController::class, 'store']);
+// Route::put('/category/{id}', [CategoryApiController::class, 'update']);
+// Route::delete('/category/{id}', [CategoryApiController::class, 'destroy']);
+
+Route::apiResource('category', CategoryApiController::class);
+
+Route::apiResource('item', ItemApiController::class);
